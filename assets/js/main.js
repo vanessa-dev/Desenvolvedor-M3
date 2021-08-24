427f0ee155e46c40bc38055e978f8f36c343b2be
Vanessa Santana <vanessah4money@gmail.com>
2021-08-24 00:03:11 -0300

    feat: funcionalidade de fachar o filtro

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
-let filtros = document.querySelectorAll('.filtros__cabecalho');
+let filtros = document.querySelectorAll('.filtros__cabecalho .filtros__titulo');
+let btnClose = document.querySelectorAll('.filtros__cabecalho .filtros__fechar');
 
 filtros.forEach((filtro) => {
     filtro.onclick = function openFiltro() {
-       this.classList.add("filtros__cabecalho--active");
+       this.parentNode.classList.add("filtros__cabecalho--active");
+    }
+});
+
+btnClose.forEach((btn) => {
+    btn.onclick = function closeFiltro() {
+       this.parentNode.classList.remove("filtros__cabecalho--active");
     }
 });
 
