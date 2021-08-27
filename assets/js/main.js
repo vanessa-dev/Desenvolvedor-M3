ea28fde6e35316c833c15ac22435fc73dcca7a39
Vanessa Santana <vanessah4money@gmail.com>
2021-08-26 23:57:07 -0300

    feat: adicionando funcionalidade de mostra/esconder detalhes no filtro.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 let filtros = document.querySelectorAll('.filtros__cabecalho .filtros__titulo');
 let btnClose = document.querySelectorAll('.filtros__cabecalho .filtros__fechar');
+let btnDetalhes = document.querySelectorAll('.filtros__cabecalho .sanfona__titulo');
 
 filtros.forEach((filtro) => {
     filtro.onclick = function openFiltro() {
@@ -10,6 +11,22 @@ filtros.forEach((filtro) => {
 btnClose.forEach((btn) => {
     btn.onclick = function closeFiltro() {
        this.parentNode.classList.remove("filtros__cabecalho--active");
+    }
+});
+
+btnDetalhes.forEach((detalhes) => {
+    detalhes.onclick = function toggleDetalhes() {
+        if(this.querySelector('i').classList.contains('fa-plus')){
+            this.nextElementSibling.style.display = "flex";
+            this.querySelector('i').classList.remove("fa-plus");
+            this.querySelector('i').classList.add("fa-minus");
+            return;
+        }
+
+        this.nextElementSibling.style.display = "none";
+        this.querySelector('i').classList.remove("fa-minus");
+        this.querySelector('i').classList.add("fa-plus");
+        
     }
 });
 
