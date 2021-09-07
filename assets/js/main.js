3748d048b3d6519be0eddbef5857e67867d0778e
Vanessa Santana <vanessah4money@gmail.com>
2021-09-06 23:55:39 -0300

    feat: adicionando produto no carrinho

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 let filtros = document.querySelectorAll('.filtros__cabecalho .filtros__titulo');
 let btnClose = document.querySelectorAll('.filtros__cabecalho .filtros__fechar');
 let btnDetalhes = document.querySelectorAll('.filtros__cabecalho .sanfona__titulo');
+let btnComprar = document.querySelectorAll('button.produtos__btn__comprar');
+let sacola = document.querySelector('.cabecalho__qtd');
 
 filtros.forEach((filtro) => {
     filtro.onclick = function openFiltro() {
@@ -27,6 +29,17 @@ btnDetalhes.forEach((detalhes) => {
         this.querySelector('i').classList.remove("fa-minus");
         this.querySelector('i').classList.add("fa-plus");
         
+    }
+});
+
+btnComprar.forEach((pedido) => {
+    pedido.onclick = function comprar() {
+       qtd = + sacola.dataset.pedidos;
+       qtd ++;
+    
+       sacola.innerText = qtd;
+       sacola.dataset.pedidos = qtd;
+
     }
 });
 
