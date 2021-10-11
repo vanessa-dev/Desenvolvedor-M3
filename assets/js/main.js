cae213abc5040c0f35fbeb978ec2b562059d2497
Vanessa Santana <vanessah4money@gmail.com>
2021-10-11 14:14:47 -0300

    feat: comecando a filtrar por todos os filtros

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,59 +4,93 @@ let btnDetalhes = document.querySelectorAll('.filtros__cabecalho .sanfona__titul
 let btnComprar = document.querySelectorAll('button.produtos__btn__comprar');
 let sacola = document.querySelector('.cabecalho__qtd');
 let produtos = document.querySelector('.produtos');
-let checkboxCores = document.querySelectorAll('.filter__container.cores input');
-let filtrosSelecionados = [];
+let checkboxTamanho = document.querySelectorAll('.filter__container.tamanhos input');
+let filtrosInput = document.querySelectorAll('.filter__container input');
+let filtrosCores = [];
+let filtroTamanho = "";
+let filtrosPreco = [];
 
-async function filtrarProdutos(filtro){
+async function filtrarProdutos(cores,tamanho,preco){
     var url = window.location.origin + '/dados/produtos.json';
     const response = await fetch(url);
     const dados    = await response.json();
-    let itemProduto = '';
+    let itemProduto = "";
+    let prods = [];
 
-    if(filtro.length > 0){
-        filtro.forEach((item) =>{
+    if(cores.length > 0){
+        cores.forEach((item) =>{
             dados.products.forEach((produto) =>{
-                if(item == produto.color) {
-                    itemProduto += `
-                        <div class="produtos__item">
-                            <div class="produtos__imagem">
-                                <img src="${produto.src}" alt="Blusa"/>
-                            </div>
-                            <div class="produtos__descricao">
-                                <p class="produtos__nome">${produto.title}</p>
-                                <p class="produtos__preco">${produto.price}</p>
-                                <p class="produtos__desconto">até 3x de R$ 9,33</p>
-                            </div>
-                            <button class="produtos__btn__comprar">Comprar</button>
-                        </div>
-                    `;
+                if (produto.color  == item)  {
+                    prods.push(produto);
                 }
                 
             });
-    
-            if(itemProduto){
-                produtos.innerHTML = itemProduto;
-            } else {
-                produtos.innerHTML = "<p>Sua busca não encontrou resultado algum :(</p>";
-            }
+        });
+    } else if (preco.length > 0) {
+        preco.forEach((item) =>{
+            prods = dados.products.filter((produto) =>{
+                if (produto.price >= item[0] && produto.price <= item[1]) {
+                    prods.push(produto);
+                }
+                
+            });
             
         });
+    } else {
+        prods = dados.products.filter((produto) =>{
+            if (produto.size ==  tamanho) {
+                prods.push(produto);
+            }  
+        });
+    }
+    console.log(prods);
+    if (prods.length > 0) {
+       prods.forEach((produto) => {
+            itemProduto += `
+                <div class="produtos__item">
+                    <div class="produtos__imagem">
+                        <img src="${produto.src}" alt="Blusa"/>
+                    </div>
+                    <div class="produtos__descricao">
+                        <p class="produtos__nome">${produto.title}</p>
+                        <p class="produtos__preco">${produto.price}</p>
+                        <p class="produtos__desconto">até ${produto.installments.times}x de R$ ${produto.installments.price.toLocaleString('pt-br', {minimumFractionDigits: 2})}</p>
+                    </div>
+                    <button class="produtos__btn__comprar">Comprar</button>
+                </div>
+            `;
+       });
 
-        return;
+       produtos.innerHTML = itemProduto;
+    } else {
+        produtos.innerHTML = "<p>Sua busca não encontrou resultado algum :(</p>"; 
     }
     
-   carregarDados();  
+//    carregarDados();  
 };
 
-checkboxCores.forEach((filtrar) => {
+filtrosInput.forEach((filtrar) => {
     filtrar.onchange = async function filtrar() {
-        if(this.checked){
-          filtrosSelecionados.push(this.id);
-        }else{
-            filtrosSelecionados.splice(filtrosSelecionados.indexOf(this.value), 1);
-        }
+        let name = this.getAttribute("name");
+        if(name == "tamanho") {
+            if (this.checked){
+                filtrosTamanho= this.value;
+            }
+        } else if( name.includes("faixa")) {
+            if (this.checked){
+                filtrosPreco.push([this.min,this.max]);
+            } else {
+                filtrosPreco.splice(filtrosPreco.indexOf(this.value), 1);
+            }
+        }  else{
+            if (this.checked){
+                filtrosCores.push(this.id);
+            } else {
+                filtrosCores.splice(filtrosCores.indexOf(this.value), 1);
+            }
+        }      
 
-        await filtrarProdutos(filtrosSelecionados);
+        await filtrarProdutos(filtrosCores,filtroTamanho,filtrosPreco);
     }
 });
 
@@ -97,6 +131,19 @@ btnComprar.forEach((pedido) => {
        sacola.innerText = qtd;
        sacola.dataset.pedidos = qtd;
 
+    }
+});
+
+checkboxTamanho.forEach((input) => {
+    input.onclick = function active () {
+        checkboxTamanho.forEach((removerClass) => {
+            removerClass.parentNode.classList.remove("item-ativo");
+        });
+
+        if(this.checked){
+            this.parentNode.classList.add("item-ativo");
+        }
+    
     }
 });
 
